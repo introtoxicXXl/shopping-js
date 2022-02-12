9616d6c7a258e63b21c320622b118e2d82515e52
introtoxicXXl <minhajulabedin648@gmail.com>
2022-02-13 01:49:11 +0600

    button function add

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('I am in a separate file')
+//add phone input number
+document.getElementById('iphone-plus').addEventListener('click',function(){
+  updateProductNum('iphone',1219,true);
+})
+document.getElementById('iphone-minus').addEventListener('click',function(){
+  updateProductNum('iphone',1219,false);
+})
+//add case input number
+document.getElementById('case-plus').addEventListener('click',function(){
+  updateProductNum('case',59,true);
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+  updateProductNum('case',59,false);
+})
+//common function for ipnone and case button
+function updateProductNum(product,price,isIncreasing){
+  const productInput = document.getElementById(product + '-input');
+  let productNumber = productInput.value;
+  if(isIncreasing == true){
+    productNumber = parseInt(productNumber) + 1;
+  }else if(productNumber > 0){
+    productNumber = parseInt(productNumber) - 1;
+  }
+  productInput.value = productNumber;
+  const productTotal = document.getElementById(product + '-total');
+  productTotal.innerText = productNumber * price;
+}
+// minus input number
+function inputNumMinus(id){
+  
+}
